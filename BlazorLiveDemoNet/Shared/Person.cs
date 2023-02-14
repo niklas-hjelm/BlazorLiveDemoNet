ace9e53b0069e5d1730cdc740c1b0c6c829f9c93
niklas-hjelm <niklas.hjelm@iths.se>
2023-02-14 15:01:44 +0100

    Add project files.

---
+++ BlazorLiveDemoNet/Shared/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorLiveDemoNet.Shared;
+
+public class Person
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
