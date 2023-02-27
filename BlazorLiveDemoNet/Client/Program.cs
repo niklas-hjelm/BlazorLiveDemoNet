a308a7a9752b7696a46c4492c369234e2097f945
niklas-hjelm <niklas.hjelm@iths.se>
2023-02-27 15:36:24 +0100

    ahaha

--- BlazorLiveDemoNet/Client/Program.cs
+++ BlazorLiveDemoNet/Client/Program.cs
@@ -1,10 +1,17 @@
 using BlazorLiveDemoNet.Client;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSessionStorageServices();
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
